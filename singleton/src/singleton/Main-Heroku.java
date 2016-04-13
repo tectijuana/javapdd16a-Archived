6cf01145daabd826b635329a03ae3588946469f4
Neoceryx <dani-deoz@hotmail.com>
2016-04-12 22:58:57 -0700

    Fierro Najera Daniel-Singleton

---
+++ singleton/src/singleton/Main-Heroku.java
@@ -0,0 +1,108 @@
+import java.sql.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static spark.Spark.*;
+import spark.template.freemarker.FreeMarkerEngine;
+import spark.ModelAndView;
+import static spark.Spark.get;
+
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
+
+class SingleObject {
+
+   //create an object of SingleObject
+   private static SingleObject instance = new SingleObject();
+
+   //make the constructor private so that this class cannot be
+   //instantiated
+   private SingleObject(){}
+   
+     //Get the only object available
+   public static SingleObject getInstance(){
+      return instance;
+   }
+
+ 
+   public void showMessage(){
+ get("/hello", (req, res) -> "Singleton Pattern says: HELLO!");
+get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Singleton Pattern says: HELLO!");
+
+            return new ModelAndView(attributes, "index.ftl");
+        }, new FreeMarkerEngine());
+    
+   }
+}
+
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    port(Integer.valueOf(System.getenv("PORT")));
+    staticFileLocation("/public");
+
+  SingleObject object = SingleObject.getInstance();
+
+      //show the message
+      object.showMessage();
+
+    get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+
+            return new ModelAndView(attributes, "index.ftl");
+        }, new FreeMarkerEngine());
+
+    get("/db", (req, res) -> {
+      Connection connection = null;
+      Map<String, Object> attributes = new HashMap<>();
+      try {
+        connection = getConnection();
+
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+        ArrayList<String> output = new ArrayList<String>();
+        while (rs.next()) {
+          output.add( "Read from DB: " + rs.getTimestamp("tick"));
+        }
+
+        attributes.put("results", output);
+        return new ModelAndView(attributes, "db.ftl");
+      } catch (Exception e) {
+        attributes.put("message", "There was an error: " + e);
+        return new ModelAndView(attributes, "error.ftl");
+      } finally {
+        if (connection != null) try{connection.close();} catch(SQLException e){}
+      }
+  }, new FreeMarkerEngine());
+
+  }
+
+  private static Connection getConnection() throws URISyntaxException, SQLException {
+    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+    int port = dbUri.getPort();
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
+
+    if (dbUri.getUserInfo() != null) {
+      String username = dbUri.getUserInfo().split(":")[0];
+      String password = dbUri.getUserInfo().split(":")[1];
+      return DriverManager.getConnection(dbUrl, username, password);
+    } else {
+      return DriverManager.getConnection(dbUrl);
+    }
+  }
+
+}
