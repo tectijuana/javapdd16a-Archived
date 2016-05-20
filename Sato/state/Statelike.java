41ceca302429efe82f1c6cb5c0057186eb6d6610
alankasunari <satoaguilaralankasunari@gmail.com>
2016-05-20 11:35:11 -0700

    Create Statelike.java

---
+++ Sato/state/Statelike.java
@@ -0,0 +1,10 @@
+interface Statelike {
+
+    /**
+     * Writer method for the state name.
+     * @param STATE_CONTEXT
+     * @param NAME
+     */
+    void writeName(final StateContext STATE_CONTEXT, final String NAME);
+   
+}
