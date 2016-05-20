112d4b3e966f6bc8269de6b11c57a53848a11715
alankasunari <satoaguilaralankasunari@gmail.com>
2016-05-20 11:35:53 -0700

    Create StateA.java

---
+++ Sato/state/StateA.java
@@ -0,0 +1,11 @@
+class StateA implements Statelike {
+    /* (non-Javadoc)
+     * @see state.Statelike#writeName(state.StateContext, java.lang.String)
+     */
+    @Override
+    public void writeName(final StateContext STATE_CONTEXT, final String NAME) {
+        System.out.println(NAME.toLowerCase());
+        STATE_CONTEXT.setState(new StateB());
+    }
+
+}
