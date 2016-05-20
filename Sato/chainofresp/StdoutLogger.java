0566f2184a938a99f5a442053695b06d05d61768
alankasunari <satoaguilaralankasunari@gmail.com>
2016-05-20 12:13:34 -0700

    Create StdoutLogger.java

---
+++ Sato/chainofresp/StdoutLogger.java
@@ -0,0 +1,11 @@
+class StdoutLogger extends Logger {
+
+    public StdoutLogger(int mask) {
+        super(mask);
+    }
+
+    protected void writeMessage(String msg) {
+        System.out.println("Writing to stdout: " + msg);
+    }
+
+}
