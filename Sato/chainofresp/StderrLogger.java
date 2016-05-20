437a07b149bf50cb9c5e606e8c60e69eec4d4022
alankasunari <satoaguilaralankasunari@gmail.com>
2016-05-20 12:14:06 -0700

    Create StderrLogger.java

---
+++ Sato/chainofresp/StderrLogger.java
@@ -0,0 +1,11 @@
+class StderrLogger extends Logger {
+
+    public StderrLogger(int mask) {
+        super(mask);
+    }
+
+    protected void writeMessage(String msg) {
+        System.err.println("Sending to stderr: " + msg);
+    }
+
+}
