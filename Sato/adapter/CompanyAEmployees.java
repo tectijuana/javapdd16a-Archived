23362050d234f1f19a1784d993a33b3d4d7e7c3b
alankasunari <satoaguilaralankasunari@gmail.com>
2016-05-20 10:19:24 -0700

    Create CompanyAEmployees.java

---
+++ Sato/adapter/CompanyAEmployees.java
@@ -0,0 +1,18 @@
+**
+ 2  * Employees of the Company A.
+ 3  */
+ 4 public class CompanyAEmployees {
+ 5   /**
+ 6    * Retrieve the employee information from the database.
+ 7    *
+ 8    * @param sqlQuery The SQL query.
+ 9    * @return The employee object.
+10    */
+11   public Employee getEmployee(String sqlQuery) {
+12       Employee employee = null;
+13      
+14       // Execute the request.
+15      
+16       return employee;
+17     }
+18 }
