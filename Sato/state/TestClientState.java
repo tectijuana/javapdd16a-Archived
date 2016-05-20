96d39c1d71180d830215b17e15357398de26dfed
alankasunari <satoaguilaralankasunari@gmail.com>
2016-05-20 11:36:49 -0700

    Create TestClientState.java

---
+++ Sato/state/TestClientState.java
@@ -0,0 +1,13 @@
+public class TestClientState {
+    public static void main(String[] args) {
+        final StateContext SC = new StateContext();
+
+        SC.writeName("Monday");
+        SC.writeName("Tuesday");
+        SC.writeName("Wednesday");
+        SC.writeName("Thursday");
+        SC.writeName("Friday");
+        SC.writeName("Saturday");
+        SC.writeName("Sunday");
+    }
+}
