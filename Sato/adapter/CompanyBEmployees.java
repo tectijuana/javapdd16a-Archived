9631668fcf6cb83343f909137c8cb44de250833f
alankasunari <satoaguilaralankasunari@gmail.com>
2016-05-20 10:19:55 -0700

    Create CompanyBEmployees.java

---
+++ Sato/adapter/CompanyBEmployees.java
@@ -0,0 +1,18 @@
+/**
+ 2  * Employees of the Company B.
+ 3  */
+ 4 public class CompanyBEmployees {
+ 5   /**
+ 6    * Retrieve the employee information from the LDAP.
+ 7    *
+ 8    * @param sqlQuery The SQL query.
+ 9    * @return The employee object.
+10    */
+11   public Employee getEmployee(String distinguishedName) {
+12       Employee employee = null;
+13      
+14       // Call the LDAP.
+15      
+16       return employee;
+17     }
+18 }
